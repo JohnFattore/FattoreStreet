4a4f8430268a74245eaed2c54a13dd7b0fd5dde1
torkfat <105743305+torkfat@users.noreply.github.com>
2022-12-17 21:48:36 -0500

    admin created

--- mysite/portfolio/models.py
+++ mysite/portfolio/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Stock(models.Model):
     ticker_text = models.CharField(max_length=4)
     shares_integer = models.IntegerField()
-    costbasis_price = models.DecimalField()
+    costbasis_price = models.DecimalField(decimal_places=2, max_digits=10)
     buy_date = models.DateTimeField('date bought')
 
 class ETF(models.Model):
     ticker_text = models.CharField(max_length=4)
     shares_integer = models.IntegerField()
-    costbasis_price = models.DecimalField()
+    costbasis_price = models.DecimalField(decimal_places=2, max_digits=10)
     buy_date = models.DateTimeField('date bought')
