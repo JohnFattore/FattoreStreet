7d5163f81efe087c0953773aa1399d9e98c8adf6
torkfat <105743305+JohnFattore@users.noreply.github.com>
2022-12-27 20:11:14 -0500

    Users can register!!!!

---
+++ mysite/portfolio/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class Register(forms.Form):
+    username = forms.CharField(label='Username', max_length=100)
+    password1 = forms.CharField(label='Password', max_length=100)
+    password2 = forms.CharField(label='Match Password', max_length=100)
+    email = forms.EmailField()
+    fname = forms.CharField(label='First Name', max_length=100)
+    lname = forms.CharField(label='Last Name', max_length=100)
--- mysite/portfolio/views.py
+++ mysite/portfolio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from .forms import Register
 
 # Create your views here.
 def index(request):
@@ -11,12 +12,26 @@ def portfolio(request):
     return render(request, 'portfolio/portfolio.html')
 
 def register(request):
-    return render(request, 'portfolio/register.html')
+    # route for post, when the form is submitted
+    if request.method == 'POST':
+        # create form object from submission
+        form = Register(request.POST)
+        # if valid form, collect all data in variables
+        # TO DO actually register
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            email = form.cleaned_data['email']
+            fname = form.cleaned_data['fname']
+            lname = form.cleaned_data['lname']
+            user = User.objects.create_user(username=username, password=password1, email=email, first_name=fname, last_name=lname)
+            user.save()
+            return render(request, 'portfolio/portfolio.html')
+    # if GET request, create blank register form
+    else:
+        form = Register()
+    return render(request, 'portfolio/register.html', {'form': form})
 
 def login(request):
     return render(request, 'portfolio/login.html')
-
-#def register(request):
- #   user = User.objects.create_user(username=FROM FORM, password='FROM FORM')
-   # user.save()
-  #  return render(request, 'portfolio/create_user.html')
