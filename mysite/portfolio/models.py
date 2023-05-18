ca30f4b14b56ca48d5c3f887c73705997f3e9139
torkfat <105743305+JohnFattore@users.noreply.github.com>
2023-05-18 10:58:25 -0400

    model updates

--- mysite/portfolio/models.py
+++ mysite/portfolio/models.py
@@ -19,3 +19,32 @@ class Allocation(models.Model):
     target = models.DecimalField(decimal_places=2, max_digits=5)
     def __str__(self):
         return self.ticker_text
+    
+class UserInfo(models.Model):
+    salary = models.DecimalField(decimal_places=0, max_digits=15)
+    # account choices
+    Roth_IRA = "RIRA"
+    Trad_IRA = "TIRA"
+    Retire401k = "401k"
+    Taxable = "TAXD"
+    ACCOUNT_CHOICES = [
+        (Roth_IRA, "Roth IRA"),
+        (Trad_IRA, "Trad IRA"),
+        (Retire401k, "401k"),
+        (Taxable, "Taxable"),
+    ]
+    account1 = models.CharField(
+        max_length=4,
+        choices=ACCOUNT_CHOICES,
+        default=Taxable,
+    )
+    account2 = models.CharField(
+        max_length=4,
+        choices=ACCOUNT_CHOICES,
+        default=Taxable,
+    )    
+    account3 = models.CharField(
+        max_length=4,
+        choices=ACCOUNT_CHOICES,
+        default=Taxable,
+    )
