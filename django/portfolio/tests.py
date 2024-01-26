32b66e889ad9ee8850557f78ba24208916c72f59
John Fattore <105743305+JohnFattore@users.noreply.github.com>
2024-01-25 20:31:58 -0600

    ava here

--- django/portfolio/tests.py
+++ django/portfolio/tests.py
@@ -1,37 +1,5 @@
-from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
-from rest_framework.test import APITestCase
-from rest_framework import status
-from django.contrib.auth.models import User
-from portfolio.models import Asset
-from portfolio.serializers import AssetSerializer 
-
-class AssetRetrieveDestroyViewTests(APITestCase):
-    def setUp(self):
-        # Create a user for testing purposes
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-
-        # Create an asset owned by the user
-        self.asset = Asset.objects.create(ticker='VTI', 
-                                          shares=10, 
-                                          costbasis=200, 
-                                          buy='2023-10-13', 
-                                          user=self.user)
-
-        # Set up authentication for the client
-        # self.client.force_authenticate(user=user, token=user.auth_token)
-
-    def test_retrieve_asset(self):
-        url = f'/api/asset/{self.asset.id}/'
-        response = self.client.get(url)
-        print("The URL is:" + self.asset.buy)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, AssetSerializer(self.asset).data)
-
-    def test_delete_asset(self):
-        url = f'/api/asset/{self.asset.id}/'
-        response = self.client.delete(url)
-
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(Asset.objects.filter(id=self.asset.id).exists())
+# Using the standard RequestFactory API to create a form POST request
+factory = APIRequestFactory()
+request = factory.post('/api/asset/1', {'title': 'new idea'})
