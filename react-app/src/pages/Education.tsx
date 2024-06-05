5e8786328d169ef78b6c38624bdad8c241f39650
John Fattore <105743305+JohnFattore@users.noreply.github.com>
2024-06-05 07:25:36 -0500

    Content

--- react-app/src/pages/Education.tsx
+++ react-app/src/pages/Education.tsx
@@ -8,8 +8,16 @@ export default function Education() {
                     Holding for an extending period of time averages out the good and bad years, resulting in an average return, which is pretty solid at 8-10%. 
                     One could double their money about every 10 years, or 4X in 20 years, or 16x in 40 years!</li>
                 <li>Keep it Simple. A 3 fund portfolio of the S&P 500, international stock, and bonds keeps the portfolio simple and maximizes returns</li>
-                <li>Don't pick individual stocks. Even professional traders struggle to beat the market (proof). You are not Warren Buffett</li>
-                <li>Dollar cost average / automate investing. Periodically buying into funds will ensure an good average cost basis and makes it simple</li>
+                <li>High Yield Savings Accounts</li>
+                <li>A stock is a small ownership in a company. Buying small parts of a company yields future returns because you are investing something that makes money. More established companies even give out a little money each quarter in the form of a dividend to repay the shareholder. Companies carry risk to account for these great gains, which means the investment takes time to mature and show its full return potential.</li>
+                <li>Don't pick individual stocks. Even professional traders struggle to beat the market (proof). Active fund managers struggle to beat the market in the long run. 
+                    A big reason for this is the large expense ratios these funds charge. If a fund charge 1.5% a year, that means the fund needs to outperform the market by 1.5% to break even.
+                    These fund managers struggle to even beat the market without this mark up. Good passive funds charge essentially nothing (0.05%). You are not Warren Buffett.</li>
+                <li>Dollar cost average / automate investing. Periodically buying into funds will ensure an good average cost basis and makes it simple
+                    An interactive tool would be cool to show potential returns over time. I started buying in 2015, 5 years what do I have now.
+                    Portfolio can also have some default vaules
+                    Paper trader!
+                </li>
                 <li>Tax advantage accounts / Save for retirement first. Take advantage of tax advantage accounts and save for retirement first</li>
                 <li>Bad debt. Any debt larger than a money market account I would consider bad debt. Pay this off first before investing. Paying off debt can be seen as a guaranteed return</li>
             </ul>
--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -1,10 +1,13 @@
-import About from '../components/About';
 import SpikeHead from '../components/SpikeHeadImg';
 
 export default function Home() {
     return (
         <>
-            <About/>
+        <p>"By periodically investing in an index fund, for example, the know-nothing investor can actually out-perform most investment professionals. Paradoxically, when ‘dumb’ money acknowledges its limitations, it ceases to be dumb." - Warren Buffett</p>
+            <h3>The goal of Fattore Street is to get all workers of America to secure their financial future. <br></br>
+                Automatically dollar cost averaging into funds that track diverse indexes such as the S&P 500 provides workers with a hands off approach to grow their money. <br></br>
+                Taking advantage of taxable accounts such as 401k and Roth IRA keeps more money in your pocket come retirement. <br></br>
+                </h3>
             <SpikeHead/>
         </>
     );
