319d2ce66c5f1113560433af19ad4dbb9b13a797
John Fattore <105743305+JohnFattore@users.noreply.github.com>
2024-12-10 07:19:27 -0600

    certbot

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -2,14 +2,6 @@
 # network
 sudo docker network create --driver bridge dockerNet
 
-# django RDS
-sudo docker run --name django --network dockerNet -d \
-  -e HOST=postgres.cpqoakmk692s.us-east-1.rds.amazonaws.com  \
-  -e USERNAME=postgres \
-  -e PASSWORD_RDS=OOjf7vvsOd0zaPTK3jhh \
-  -e DATABASE=postgresRDS \
-  johnfattore/django
-
 # postgres
 sudo docker run -d \
   --name postgres \
@@ -25,8 +17,6 @@ sudo docker run --name django --network dockerNet -d \
   -e DEBUG=False \
   johnfattore/django
 
-
-
 # nginx
 sudo docker run --name nginx --network dockerNet -p 80:80 -d johnfattore/nginx
 
@@ -35,6 +25,18 @@ docker run -it --rm --name certbot --network dockerNet -p 80:80 -p 443:443 -d ce
 
 # Copy into volume
 docker run --rm -v volume_name:/container_path -v $(pwd):/host busybox cp /host/yourfile /container_path/yourfile
+
+# Certbot
+docker run --rm \
+  -v /path/to/certbot/conf:/etc/letsencrypt \
+  -v /path/to/certbot/www:/var/www/certbot \
+  certbot/certbot certonly \
+  --webroot \
+  -w /var/www/certbot \
+  -d yourdomain.com -d www.yourdomain.com
+
+# nginx with Certbot
+sudo docker run --name nginx --network dockerNet -p 80:80 -p 443:443 -d johnfattore/nginx
 
 # Plan
 # Copy certbot files from certbot container
@@ -55,3 +57,11 @@ sudo docker image rm johnfattore/django
 sudo docker stop nginx
 sudo docker container rm nginx
 sudo docker image rm johnfattore/nginx
+
+# django RDS
+sudo docker run --name django --network dockerNet -d \
+  -e HOST=postgres.cpqoakmk692s.us-east-1.rds.amazonaws.com  \
+  -e USERNAME=postgres \
+  -e PASSWORD_RDS=OOjf7vvsOd0zaPTK3jhh \
+  -e DATABASE=postgresRDS \
+  johnfattore/django
