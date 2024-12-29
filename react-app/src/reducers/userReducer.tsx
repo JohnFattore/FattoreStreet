dcc2dd0fe80120411fe10c39ee43815015186898
torkfat <105743305+JohnFattore@users.noreply.github.com>
2024-12-29 09:54:47 -0600

    Update userReducer.tsx

--- react-app/src/reducers/userReducer.tsx
+++ react-app/src/reducers/userReducer.tsx
@@ -1,15 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { login } from '../components/axiosFunctions';
 
 interface UserSlice {
   loading: boolean;
-  user: string,
+  username: string,
   token: string,
   error: string;
 }
 
 const initialState: UserSlice = {
   loading: false,
-  user: '',
+  username: '',
   token: '',
   error: '',
 };
@@ -18,6 +19,21 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+    extraReducers: (builder) => {
+      builder
+        .addCase(login.pending, (state) => {
+          state.loading = true;  // Set loading to true when the async call is pending
+        })
+        .addCase(login.fulfilled, (state, action) => {
+          state.loading = false;  // Set loading to false when the async call is fulfilled
+          state.username = action.payload.username;
+          state.token = action.payload.token;
+        })
+        .addCase(login.rejected, (state, action) => {
+          state.loading = false;  // Set loading to false if the call failed
+          state.error = action.error.message || 'An error occurred';  // Set the error message
+        })
+    }
 });
 
 export default userSlice.reducer;
