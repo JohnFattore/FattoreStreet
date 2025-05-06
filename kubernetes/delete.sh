e6482e736d10d9a8e906435cca1ffb6d6553ea23
John Fattore <105743305+JohnFattore@users.noreply.github.com>
2025-05-05 19:57:02 -0500

    Stop tracking kubernetes folder

--- kubernetes/delete.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-kubectl delete -n default service nginx-service
-kubectl delete -n default service django-service
-kubectl delete -n default deployment nginx-deployment
-kubectl delete -n default deployment django-deployment
